209c2ee0255aca711757621f6b5440ddee6032e3
WolfProgrammerPro <161871668+WolfProgrammerPro@users.noreply.github.com>
2025-10-25 19:27:41 +0500

    Update main.js

--- main.js
+++ main.js
@@ -10,4 +10,10 @@ if (isMobile)
         image.style.width = '80%'
         image.style.left = '5%'
     }
+    const how_to_use = document.getElementById('how-to-use')
+    how_to_use.style.top = '72%'
+    const ol_1 = document.getElementById('ol-1')
+    ol_1.style.top = '77%'
+    const img1 = document.getElementById('img-1')
+    img1.style.top = '93%'
 }
