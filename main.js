e889d6aebea38eaad230d1aa065e664a354186ac
WolfProgrammerPro <161871668+WolfProgrammerPro@users.noreply.github.com>
2025-10-26 10:11:18 +0500

    Update main.js

--- main.js
+++ main.js
@@ -7,32 +7,54 @@ if (isMobile)
     for (let i = 0; i < images.length; i++)
     {
         const image = images[i]
-        console.log('aaa')
-        image.style.width = '80%'
-        image.style.left = '5%'
+        if (image.id != "back" && image.id != "ava")
+        {
+            image.style.width = '80%'
+            image.style.left = '5%'
+        }
+        
     }
+    const h1 = document.getElementById('h1')
+    h1.style.top = '15%'
+     const what = document.getElementById('what')
+    what.style.top = '25%'
+    what.style.width = "70%"
     const how_to_use = document.getElementById('how-to-use')
     how_to_use.style.top = '72%'
+    how_to_use.style.width = '70%'
     const ol_1 = document.getElementById('ol-1')
-    ol_1.style.top = '77%'
+    ol_1.style.top = '80%'
+    ol_1.style.width = '70%'
     const img1 = document.getElementById('img-1')
-    img1.style.top = '93%'
+    img1.style.top = '97%'
     const ol2 = document.getElementById('ol-2')
-    ol2.style.top = '116%'
+    ol2.style.top = '120%'
+    ol2.style.width = '70%'
     const img2 = document.getElementById('img-2')
-    img2.style.top = '130%'
+    img2.style.top = '135%'
     const p1 = document.getElementById('p1')
-    p1.style.top = '160%'
+    p1.style.top = '180%'
     p1.style.left = "7%"
-    p1.style.width = "85%"
+    p1.style.width = "70%"
+
     const ol3 = document.getElementById('ol-3')
     ol3.style.top = '175%'
+    ol3.style.width = '70%'
     const img3 = document.getElementById('img-3')
     img3.style.top = '210%'
     const ol4 = document.getElementById('ol-4')
     ol4.style.top = '240%'
+    ol4.style.width = '70%'
     const img4 = document.getElementById('img-4')
     img4.style.top = '290%'
     const END = document.getElementById('end')
     END.style.top = '370%'
+    const ava = document.getElementById('ava')
+    ava.style.left = '85%'
+    const back = document.getElementById('back')
+    back.style.height = '380%'
+    const plus = document.getElementById('plus')
+    plus.style.width = '30%'
+    const ul1 = document.getElementById('ul1')
+    ul1.style.width = '70%'
 }
